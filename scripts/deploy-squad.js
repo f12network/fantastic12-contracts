e0f1dd0e0bd804c16b6ca720ed13c556e1352773
Zefram Lou <zeframlou@gmail.com>
2020-01-07 16:35:41 -0800

    fixed deploy-squad.js

--- scripts/deploy-squad.js
+++ scripts/deploy-squad.js
@@ -2,6 +2,7 @@
 // when running the script with `buidler run <script>`: you'll find the Buidler
 // Runtime Environment's members available as global variable in that case.
 const env = require("@nomiclabs/buidler");
+const BigNumber = require("bignumber.js");
 
 async function main() {
   // You can run Buidler tasks from a script.
@@ -15,8 +16,9 @@ async function main() {
   console.log(`Deployed Fantastic12Factory at address ${factory.address}`);
 
   // Deploy squad
-  const withdrawLimit = `${20 * PRECISION}`;
-  const consensusThreshold = `${0.75 * PRECISION}`;
+  const PRECISION = 1e18;
+  const withdrawLimit = BigNumber(1000 * PRECISION);
+  const consensusThreshold = BigNumber(0.75 * PRECISION);
   const result = await factory.createSquad(accounts[0], withdrawLimit, consensusThreshold);
   const squadAddress = result.logs[0].args.squad;
   console.log(`Deployed Fantastic12 squad at address ${squadAddress}`);
